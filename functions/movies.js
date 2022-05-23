71dd67b3d6afe920d52f5602268964ec0761130e
Kevin Roney <ktroney.pdx@gmail.com>
2022-05-23 14:53:07 -0700

    added fetch link and key to Movies.js

--- functions/movies.js
+++ functions/movies.js
@@ -9,7 +9,7 @@ const headers = {
 
 exports.handler = async (event, context) => {
   try {
-    const response = await fetch('');
+    const response = await fetch(`https://api.themoviedb.org/3/search/movie?=${event.queryStringParameters.movie}api_key=${process.env.MOVIE_KEY}&language=en-US&page=1&include_adult=false`);
     const data = await response.json();
     const json = JSON.stringify(data);
 

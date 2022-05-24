40353f57c0bc1e1e68c647e227643f2236b62543
Beth <bethmelmtv@gmail.com>
2022-05-24 13:05:40 -0700

    moved fetch code

--- src/WatchList.js
+++ src/WatchList.js
@@ -1,29 +1,29 @@
 import React, { useEffect, useState } from 'react';
-import { 
-  getMovies,
-  removeMovie,
-} from './services/SupabaseUtils';
+import { getMovies, removeMovie } from './services/SupabaseUtils';
 import MovieCard from './MovieCard';
-
 
 export default function WatchList() {
   const [favMovies, setFavMovies] = useState([]);
+
+  async function fetch() {
+    const movieData = await getMovies();
+    setFavMovies(movieData);
+  }
   useEffect(() => {
-    async function fetch() {
-      const movieData = await getMovies();
-      setFavMovies(movieData);
-    }
     fetch();
   }, []);
   async function handleDelete(id) {
-    
     await removeMovie(id);
-    await getMovies();
+    await fetch();
   }
-  return <div>
-    {favMovies.map((movie, i) =>
-      <><MovieCard key={movie.id + movie.title + i} {...movie}/>
-        <button onClick={()=> handleDelete(movie.id)}>Watched</button>
-      </>)}
-  </div>;
+  return (
+    <div>
+      {favMovies.map((movie, i) => (
+        <>
+          <MovieCard key={movie.id + movie.title + i} {...movie} />
+          <button onClick={() => handleDelete(movie.id)}>Watched</button>
+        </>
+      ))}
+    </div>
+  );
 }

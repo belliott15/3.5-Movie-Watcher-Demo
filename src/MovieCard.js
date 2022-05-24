68838c6b1fd05f11dee7b46c4e43d2ca422804c1
Beth <bethmelmtv@gmail.com>
2022-05-23 17:20:14 -0700

    worked on moviecard.js

--- src/MovieCard.js
+++ src/MovieCard.js
@@ -1,7 +1,16 @@
 import React from 'react';
+import { fetchMovies } from './services/fetch-utils';
 
-export default function MovieCard() {
+export default function MovieCard({ movies }) {
   return (
-    <div>MovieCard</div>
+    <div>
+      {movies.map((movie, i) => (
+        <div key={movie.id + movie.title}>
+          <p>{movie.original_title}</p>
+          <p>{movie.year}</p>
+          <img src={`https://image.tmdb.org/t/p/original${movie.poster_path}`} />
+        </div>
+      ))}
+    </div>
   );
 }
--- src/Search.js
+++ src/Search.js
@@ -4,18 +4,19 @@ import MovieCard from './MovieCard';
 export default function Search() {
   const [search, setSearch] = useState('');
   const [result, setResults] = useState([]);
-
-  async function handleSearch(e){
+  console.log(result);
+  async function handleSearch(e) {
     e.preventDefault();
     const movies = await fetchMovies(search);
     setResults(movies.results);
   }
-  return <div>
-    <form onSubmit={handleSearch}>
-      <input value={search}
-        onChange={e => setSearch(e.target.value)}/>
-      <button>Search</button>
-    </form>
-    <MovieCard movies={result}/>
-  </div>;
+  return (
+    <div>
+      <form onSubmit={handleSearch}>
+        <input value={search} onChange={(e) => setSearch(e.target.value)} />
+        <button>Search</button>
+      </form>
+      <MovieCard movies={result} />
+    </div>
+  );
 }
